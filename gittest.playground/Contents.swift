452898492b9dc8ca6c7bf8283c020eddacd02a9f
Choolwe Muleya <choolwemuleya@gmail.com>
2023-06-13 14:52:28 +0200

    we added a subtraction function

--- gittest.playground/Contents.swift
+++ gittest.playground/Contents.swift
@@ -18,3 +18,11 @@ func mult(_ num1: Int, _ num2: Int) -> Int {
 }
 
 print(mult(10, 25))
+
+// subtract function
+func sub(_ num1: Int, _ num2: Int) -> Int{
+    
+    return num1 - num2
+}
+
+print(sub(20, 15))
